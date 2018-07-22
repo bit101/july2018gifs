a2c80ae40b89a7a6de21264d0540b1912391fd5d
Keith Peters <kp@bit-101.com>
2018-07-22 10:06:59 -0400

    21_22

---
+++ july/july21.go
@@ -0,0 +1,55 @@
+package july
+
+import (
+	"math"
+
+	"github.com/bit101/blgo"
+	"github.com/bit101/blgo/anim"
+	"github.com/bit101/blgo/blmath"
+	"github.com/bit101/blgo/util"
+)
+
+func July21() {
+	const (
+		outFileName   = "out/july21.gif"
+		timeInSeconds = 6
+		fps           = 30
+		frames        = timeInSeconds * fps
+		framesDir     = "frames"
+		width         = 400.0
+		height        = 400.0
+	)
+	render := func(surface *blgo.Surface, percent float64) {
+		surface.SetLineWidth(0.5)
+		surface.ClearRGB(0, 0, 0)
+		surface.SetSourceRGB(1, 1, 1)
+		amt1 := blmath.LerpSin(percent, 0, 1)
+		amt2 := blmath.LerpSin(percent+0.17, 0, 1)
+		offset := percent * math.Pi * 2
+
+		// offset := blmath.LerpSin(percent, 0, math.Pi*2)
+		for i := 0.0; i < 400.0; i++ {
+			y0 := math.Sin(i*0.2*amt1+offset)*60.0*amt1 + 70
+			surface.LineTo(i, y0)
+		}
+		surface.Stroke()
+		for i := 0.0; i < 400.0; i++ {
+			y0 := math.Sin(i*0.13+offset*20)*60.0*amt2 + 330
+			surface.LineTo(i, y0)
+		}
+		surface.Stroke()
+		surface.Stroke()
+		for i := 0.0; i < 400.0; i++ {
+			y0 := math.Sin(i*0.2*amt1+offset) * 30.0 * amt1
+			y0 += math.Sin(i*0.13+offset*20) * 30.0 * amt2
+			y0 += 200
+			surface.LineTo(i, y0)
+		}
+		surface.Stroke()
+	}
+
+	animation := anim.NewAnimation(width, height, frames)
+	animation.Render(framesDir, "frame", render)
+	util.ConvertToGIF(framesDir, outFileName, fps)
+	util.ViewImage(outFileName)
+}
---
+++ july/july22.go
@@ -0,0 +1,65 @@
+package july
+
+import (
+	"math"
+
+	"github.com/bit101/blgo"
+	"github.com/bit101/blgo/anim"
+	"github.com/bit101/blgo/util"
+)
+
+func July22() {
+	const (
+		outFileName   = "out/july22.gif"
+		timeInSeconds = 6
+		fps           = 30
+		frames        = timeInSeconds * fps
+		framesDir     = "frames"
+		width         = 400.0
+		height        = 400.0
+	)
+	render := func(surface *blgo.Surface, percent float64) {
+		surface.SetLineWidth(0.5)
+		surface.ClearRGB(0, 0, 0)
+		surface.Save()
+		surface.Translate(width/2, height/2)
+
+		offset := percent * math.Pi * 2
+
+		for i := 0.0; i < math.Pi*2; i += 0.01 {
+			r := math.Sin(i*17-offset*2)*30.0 + 168
+			x0 := math.Cos(i) * r
+			y0 := math.Sin(i) * r
+			surface.LineTo(x0, y0)
+		}
+		surface.SetSourceRGB(1, 1, 1)
+		surface.Fill()
+
+		for i := 0.0; i < math.Pi*2; i += 0.01 {
+			r := math.Sin(i*20+offset*3) * 10.0
+			r += math.Sin(i*17-offset*2) * 30.0
+			r += 100
+			x0 := math.Cos(i) * r
+			y0 := math.Sin(i) * r
+			surface.LineTo(x0, y0)
+		}
+		surface.SetSourceRGB(0, 0, 0)
+		surface.Fill()
+
+		for i := 0.0; i < math.Pi*2; i += 0.01 {
+			r := math.Sin(i*20+offset*3)*10.0 + 50
+			x0 := math.Cos(i) * r
+			y0 := math.Sin(i) * r
+			surface.LineTo(x0, y0)
+		}
+		surface.SetSourceRGB(1, 1, 1)
+		surface.Fill()
+
+		surface.Restore()
+	}
+
+	animation := anim.NewAnimation(width, height, frames)
+	animation.Render(framesDir, "frame", render)
+	util.ConvertToGIF(framesDir, outFileName, fps)
+	util.ViewImage(outFileName)
+}
